eb9dc323d0cd87403dd6549887663887d31c3dd4
Srijan Sahay <srijan05sahay@gmail.com>
2024-08-19 23:14:22 +0530

    Post create-edit, Paginatin, Minor tweaks

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
+from .views import UserPostListview, PostListview, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="blog-home"),
+    path("", PostListview.as_view(), name="blog-home"),
+    path("user/<str:username>", UserPostListview.as_view(), name="user-posts"),
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path("post/<int:pk>/update", PostUpdateView .as_view(), name="post-update"),
+    path("post/<int:pk>/delete", PostDeleteView .as_view(), name="post-delete"),
+    path("post/new/", PostCreateView.as_view(), name="post-create"),
     path("about/", views.about, name="blog-about"),
 ]
