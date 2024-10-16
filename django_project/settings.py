8ce38d890b70a6c09102ac6cc2f050b0b8490d9c
Srijan Sahay <srijan05sahay@gmail.com>
2024-10-16 12:48:46 +0530

    switched to postgreSQL

--- django_project/settings.py
+++ django_project/settings.py
@@ -81,10 +81,15 @@ WSGI_APPLICATION = "django_project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "blogsapp",
+        "USER" : "srijansahay",
+        "PASSWORD" : "test@123",
+        "HOST" : "localhost",
+        "PORT" : "5432",
+        }
     }
-}
+
 
 
 # Password validation
